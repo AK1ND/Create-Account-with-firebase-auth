8788c0e83d127390e0d72312b910584e9dc7a1f5
Course <course16062021@gmail.com>
2021-12-20 16:39:30 +0300

    Added Google Auth

--- app/src/main/java/com/alex_kind/createaccount/LoginActivity.kt
+++ app/src/main/java/com/alex_kind/createaccount/LoginActivity.kt
@@ -3,12 +3,25 @@ package com.alex_kind.createaccount
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.alex_kind.createaccount.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var laucher: ActivityResultLauncher<Intent>
+    lateinit var auth: FirebaseAuth
+
     lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,9 +29,22 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = Firebase.auth
+        auth.currentUser
+
         binding.tvRegistration.setOnClickListener {
             startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
+            onBackPressed()
         }
+
+        loginEmail()
+        loginWithGoogle()
+    }
+
+
+
+
+    private fun loginEmail() {
 
         binding.buttonLogin.setOnClickListener {
             when {
@@ -67,10 +93,84 @@ class LoginActivity : AppCompatActivity() {
                                     Toast.LENGTH_SHORT
                                 ).show()
                             }
-
                         }
                 }
             }
         }
     }
+
+    private fun loginWithGoogle() {
+
+        laucher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                if (account != null) {
+                    firebaseAuthWithGoogle(account.idToken)
+                }
+            } catch (e: ApiException) {
+                Log.d("FirebaseGoogleLog", "Api exception")
+            }
+
+        }
+
+
+        binding.buttonLoginWithGoogle.setOnClickListener {
+            signIn()
+        }
+
+        checkAuthState()
+
+
+    }
+
+    private fun getClient(): GoogleSignInClient {
+        val gso = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id)) //don't have an error
+            .requestEmail()
+            .build()
+
+        return GoogleSignIn.getClient(this, gso)
+    }
+
+    private fun signIn() {
+
+        val signIClient = getClient()
+        laucher.launch(signIClient.signInIntent)
+    }
+
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Log.d("FirebaseGoogleLog", "Google signIn done")
+                    checkAuthState()
+
+                } else {
+                    Log.d("FirebaseGoogleLog", "Google signIn incorrect")
+                }
+            }
+    }
+
+    private fun checkAuthState() {
+        if (auth.currentUser != null) {
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            intent.flags =
+                Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            intent.putExtra(
+                "userid",
+                FirebaseAuth.getInstance().currentUser?.uid
+            ) // in other activity call intent.getStringExtra("userid")
+            startActivity(intent)
+            finish()
+        }
+
+    }
+    //Google Auth end.
+
+
 }
--- app/src/main/java/com/alex_kind/createaccount/RegisterActivity.kt
+++ app/src/main/java/com/alex_kind/createaccount/RegisterActivity.kt
@@ -3,13 +3,26 @@ package com.alex_kind.createaccount
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.alex_kind.createaccount.databinding.ActivityRegisterBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
+    lateinit var laucher: ActivityResultLauncher<Intent>
+    lateinit var auth: FirebaseAuth
+
     lateinit var binding: ActivityRegisterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,11 +30,21 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = Firebase.auth
+        auth.currentUser
+
         binding.tvLogin.setOnClickListener {
             startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
+            onBackPressed()
         }
 
+        registerEmail()
+        registerWithGoogle()
 
+    }
+
+
+    private fun registerEmail() {
         binding.buttonRegister.setOnClickListener {
             when {
                 TextUtils.isEmpty(binding.editEmailRegister.text.toString().trim { it <= ' ' }) -> {
@@ -78,4 +101,80 @@ class RegisterActivity : AppCompatActivity() {
             }
         }
     }
+
+
+    private fun registerWithGoogle() {
+
+        laucher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                if (account != null) {
+                    firebaseAuthWithGoogle(account.idToken)
+                }
+            } catch (e: ApiException) {
+                Log.d("FirebaseGoogleLog", "Api exception")
+            }
+
+        }
+
+
+        binding.buttonRegisterWithGoogle.setOnClickListener {
+            signIn()
+        }
+
+        checkAuthState()
+
+
+    }
+
+    private fun getClient(): GoogleSignInClient {
+        val gso = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id)) //don't have an error
+            .requestEmail()
+            .build()
+
+        return GoogleSignIn.getClient(this, gso)
+    }
+
+    private fun signIn() {
+
+        val signIClient = getClient()
+        laucher.launch(signIClient.signInIntent)
+    }
+
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Log.d("FirebaseGoogleLog", "Google signIn done")
+                    checkAuthState()
+
+                } else {
+                    Log.d("FirebaseGoogleLog", "Google signIn incorrect")
+                }
+            }
+    }
+
+    private fun checkAuthState() {
+        if (auth.currentUser != null) {
+            val intent = Intent(this@RegisterActivity, MainActivity::class.java)
+            intent.flags =
+                Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            intent.putExtra(
+                "userid",
+                FirebaseAuth.getInstance().currentUser?.uid
+            ) // in other activity call intent.getStringExtra("userid")
+            startActivity(intent)
+            finish()
+        }
+
+    }
+    //Google Auth end.
+
+
 }
